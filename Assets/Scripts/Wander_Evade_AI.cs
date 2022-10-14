a774be685a849003c4466be5e72026242165a56f
Loproxi <79161178+Loproxi@users.noreply.github.com>
2022-10-14 14:26:07 +0200

    WanderEvadeUpgraded

--- Assets/Scripts/Wander_Evade_AI.cs
+++ Assets/Scripts/Wander_Evade_AI.cs
@@ -1,29 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Wander_Evade_AI : MonoBehaviour
 {
     [Header("Path")]
     [SerializeField] Node_Path path;
-    [Header("Ghost")]
-    [SerializeField] GameObject ghost;
     [Header("Runner")]
     [SerializeField] GameObject runner;
     [Header("Variables")]
     [Range(5.0f, 10.0f)]
     [SerializeField] float evadeArea;
-    [Range(5.0f, 10.0f)]
-    [SerializeField] float smoothArea;
+    [Range(2.0f, 5.0f)]
+    [SerializeField] float stopArea;
     [Range(2.0f, 8.0f)]
-    [SerializeField] float speed = 0.0f;
     [SerializeField] float rotationspeed = 3.0f;
-    Vector3 direction;
+    private NavMeshAgent agent;
+    [SerializeField] int currentNode = 0;
+    Node[] node;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        node = path.GetPathNodes();
     }
 
     // Update is called once per frame
@@ -32,33 +34,41 @@ public class Wander_Evade_AI : MonoBehaviour
         Vector3 followghost = Vector3.zero;
         Vector3 evasion = Vector3.zero;
 
-        if (Vector3.Distance(runner.transform.position, this.transform.position) > evadeArea)
-        {
-            //Vector From the Gameobject that has this script to Ghost
-            followghost = (ghost.transform.position - this.transform.position);
-        }
-        else
+        
+
+        if (Vector3.Distance(runner.transform.position, this.transform.position) < evadeArea)
         {
             //Vector From runner to Gameobject that has this script
             evasion = (this.transform.position - runner.transform.position);
+            agent.SetDestination(evasion);
+            
+        }
+        else
+        {
+
+            if (Vector3.Distance(node[currentNode].transform.position, this.agent.transform.position) < stopArea)
+            {
+                
+                if (currentNode == (path.GetPathNodes().Length - 1))
+                {
+                    currentNode = 0;
+                }
+                else
+                {
+                    currentNode++;
+                }
+            }
+            //Vector From the Gameobject that has this script to Ghost
+            agent.SetDestination(node[currentNode].transform.position);
         }
 
-        direction = (followghost + evasion).normalized * speed ;
-        direction.y = 0.0f;
-        
-        this.transform.position += direction * Time.deltaTime;
-
-        float angle = Mathf.Rad2Deg * Mathf.Atan2(direction.x, direction.z);
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);  // up = y
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationspeed);
-
-        
+      
 
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, evadeArea);
+        Gizmos.DrawWireSphere(transform.position, stopArea);
     }
 }
