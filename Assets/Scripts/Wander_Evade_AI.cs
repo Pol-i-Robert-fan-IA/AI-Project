23d783ab7f91f4a1dc0bea9c5bbe68bb5493236f
Loproxi <79161178+Loproxi@users.noreply.github.com>
2022-10-14 18:18:07 +0200

    WanderEvade now handles multiple runners

--- Assets/Scripts/Wander_Evade_AI.cs
+++ Assets/Scripts/Wander_Evade_AI.cs
@@ -7,8 +7,6 @@ public class Wander_Evade_AI : MonoBehaviour
 {
     [Header("Path")]
     [SerializeField] Node_Path path;
-    [Header("Runner")]
-    [SerializeField] GameObject runner;
     [Header("Variables")]
     [Range(5.0f, 10.0f)]
     [SerializeField] float evadeArea;
@@ -19,11 +17,18 @@ public class Wander_Evade_AI : MonoBehaviour
     private NavMeshAgent agent;
     [SerializeField] int currentNode = 0;
     Node[] node;
+    GameObject[] runnerArray;
+    GameObject runnertoavoid;
+    [SerializeField] int num_runners = 3;
+    [SerializeField] float speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        runnerArray = GameObject.FindGameObjectsWithTag("Runner");
+
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
 
         node = path.GetPathNodes();
     }
@@ -33,22 +38,22 @@ public class Wander_Evade_AI : MonoBehaviour
     {
         Vector3 followghost = Vector3.zero;
         Vector3 evasion = Vector3.zero;
+        agent.speed = speed;
 
-        
-
-        if (Vector3.Distance(runner.transform.position, this.transform.position) < evadeArea)
+        if (DistanceRunners())
         {
             //Vector From runner to Gameobject that has this script
-            evasion = (this.transform.position - runner.transform.position);
+            evasion = (this.transform.position - runnertoavoid.transform.position);
             agent.SetDestination(evasion);
-            
+            speed = 7.0f;
+
         }
         else
         {
 
             if (Vector3.Distance(node[currentNode].transform.position, this.agent.transform.position) < stopArea)
             {
-                
+
                 if (currentNode == (path.GetPathNodes().Length - 1))
                 {
                     currentNode = 0;
@@ -60,9 +65,9 @@ public class Wander_Evade_AI : MonoBehaviour
             }
             //Vector From the Gameobject that has this script to Ghost
             agent.SetDestination(node[currentNode].transform.position);
-        }
 
-      
+            speed = 3.5f;
+        }
 
     }
 
@@ -71,4 +76,18 @@ public class Wander_Evade_AI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, evadeArea);
         Gizmos.DrawWireSphere(transform.position, stopArea);
     }
+
+    bool DistanceRunners()
+    {
+        for(int i = 0; i <runnerArray.Length;i++)
+        {
+            if(Vector3.Distance(runnerArray[i].transform.position, this.transform.position) < evadeArea)
+            {
+                runnertoavoid = runnerArray[i];
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
