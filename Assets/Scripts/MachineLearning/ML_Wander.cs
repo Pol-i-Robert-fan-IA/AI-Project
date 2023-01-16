e5fc0d9ac305acfe98767a27aca6a2e701d91ba9
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-01-16 18:20:16 +0100

    ML_Agents commented

--- Assets/Scripts/MachineLearning/ML_Wander.cs
+++ Assets/Scripts/MachineLearning/ML_Wander.cs
@@ -1,164 +1,164 @@
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
-using Unity.MLAgents.Actuators;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.MLAgents;
+//using Unity.MLAgents.Sensors;
+//using Unity.MLAgents.Actuators;
 
-public class ML_Wander : Agent
-{
-    private Vector3 initialPos;
-    Rigidbody rBody;
+//public class ML_Wander : Agent
+//{
+//    private Vector3 initialPos;
+//    Rigidbody rBody;
 
-    public Transform Target;
-    private bool resetTargetPos = false;
+//    public Transform Target;
+//    private bool resetTargetPos = false;
 
-    [SerializeField] float maxStopTime = 10.0f;
-    [SerializeField] float currentDelay = 0.0f;
+//    [SerializeField] float maxStopTime = 10.0f;
+//    [SerializeField] float currentDelay = 0.0f;
 
-    private int strike = 0;
+//    private int strike = 0;
 
-    public override void Initialize()
-    {
-        rBody = GetComponent<Rigidbody>();
-        initialPos = transform.localPosition;
-    }
+//    public override void Initialize()
+//    {
+//        rBody = GetComponent<Rigidbody>();
+//        initialPos = transform.localPosition;
+//    }
 
-    public override void OnEpisodeBegin()
-    {
-        this.rBody.angularVelocity = Vector3.zero;
-        this.rBody.velocity = Vector3.zero;
-        //this.transform.localPosition = initialPos;
+//    public override void OnEpisodeBegin()
+//    {
+//        this.rBody.angularVelocity = Vector3.zero;
+//        this.rBody.velocity = Vector3.zero;
+//        //this.transform.localPosition = initialPos;
 
-        //Unstuck
-        //currentDelay = 0.0f;
+//        //Unstuck
+//        //currentDelay = 0.0f;
 
-        if (resetTargetPos)
-        {
-            Vector3 newPos = RandomRayCast();
-            newPos.y += 1.5f;
-            Target.position = newPos;
-            resetTargetPos = false;
-        }
+//        if (resetTargetPos)
+//        {
+//            Vector3 newPos = RandomRayCast();
+//            newPos.y += 1.5f;
+//            Target.position = newPos;
+//            resetTargetPos = false;
+//        }
 
-        if (strike != 0) Debug.Log("Strike '" + strike + "'!");
+//        if (strike != 0) Debug.Log("Strike '" + strike + "'!");
 
-    }
+//    }
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        //sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(transform.InverseTransformDirection(rBody.velocity));
+//    public override void CollectObservations(VectorSensor sensor)
+//    {
+//        //sensor.AddObservation(Target.localPosition);
+//        sensor.AddObservation(transform.InverseTransformDirection(rBody.velocity));
 
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.y);
-    }
+//        sensor.AddObservation(rBody.velocity.x);
+//        sensor.AddObservation(rBody.velocity.y);
+//    }
 
-    public override void OnActionReceived(ActionBuffers actionBuffers)
-    {
-        AddReward(-1.0f / MaxStep);
-        MoveAgent(actionBuffers.DiscreteActions);
-        UnStuck();
-    }
+//    public override void OnActionReceived(ActionBuffers actionBuffers)
+//    {
+//        AddReward(-1.0f / MaxStep);
+//        MoveAgent(actionBuffers.DiscreteActions);
+//        UnStuck();
+//    }
 
-    public void UnStuck()
-    {
-        if (rBody.velocity.z < 0.05f) currentDelay += Time.deltaTime;
-        else currentDelay = 0.0f;
+//    public void UnStuck()
+//    {
+//        if (rBody.velocity.z < 0.05f) currentDelay += Time.deltaTime;
+//        else currentDelay = 0.0f;
 
-        if (currentDelay > maxStopTime)
-        {
-            resetTargetPos = true;
-            currentDelay = 0.0f;
-            AddReward(-1.5f);
-            strike = 0;
-            Debug.Log("Unstucking");
-            EndEpisode();
-        }
-    }
+//        if (currentDelay > maxStopTime)
+//        {
+//            resetTargetPos = true;
+//            currentDelay = 0.0f;
+//            AddReward(-1.5f);
+//            strike = 0;
+//            Debug.Log("Unstucking");
+//            EndEpisode();
+//        }
+//    }
 
-    public void MoveAgent(ActionSegment<int> act)
-    {
-        Vector3 direction = Vector3.zero;
-        Vector3 rotateDir = Vector3.zero;
+//    public void MoveAgent(ActionSegment<int> act)
+//    {
+//        Vector3 direction = Vector3.zero;
+//        Vector3 rotateDir = Vector3.zero;
 
-        var action = act[0];
-        switch (action)
-        {
-            case 1: 
-                direction = transform.forward * 1f;
-                break;
-            case 2:
-                direction = transform.forward * -1f;
-                break;
-            case 3: 
-                rotateDir = transform.up * 1f;
-                break;
-            case 4: 
-                rotateDir = transform.up * -1f;
-                break;
-        }
+//        var action = act[0];
+//        switch (action)
+//        {
+//            case 1: 
+//                direction = transform.forward * 1f;
+//                break;
+//            case 2:
+//                direction = transform.forward * -1f;
+//                break;
+//            case 3: 
+//                rotateDir = transform.up * 1f;
+//                break;
+//            case 4: 
+//                rotateDir = transform.up * -1f;
+//                break;
+//        }
 
-        transform.Rotate(rotateDir, Time.deltaTime * 200f);
-        rBody.AddForce(direction * 2f, ForceMode.VelocityChange);
-    }
+//        transform.Rotate(rotateDir, Time.deltaTime * 200f);
+//        rBody.AddForce(direction * 2f, ForceMode.VelocityChange);
+//    }
 
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var discreteActionsOut = actionsOut.DiscreteActions;
-        if (Input.GetKey(KeyCode.D))
-        {
-            discreteActionsOut[0] = 3;
-        } else if (Input.GetKey(KeyCode.W))
-        {
-            discreteActionsOut[0] = 1;
-        } else if (Input.GetKey(KeyCode.A))
-        {
-            discreteActionsOut[0] = 4;
-        } else if (Input.GetKey(KeyCode.S))
-        {
-            discreteActionsOut[0] = 2;
-        }
-    }
+//    public override void Heuristic(in ActionBuffers actionsOut)
+//    {
+//        var discreteActionsOut = actionsOut.DiscreteActions;
+//        if (Input.GetKey(KeyCode.D))
+//        {
+//            discreteActionsOut[0] = 3;
+//        } else if (Input.GetKey(KeyCode.W))
+//        {
+//            discreteActionsOut[0] = 1;
+//        } else if (Input.GetKey(KeyCode.A))
+//        {
+//            discreteActionsOut[0] = 4;
+//        } else if (Input.GetKey(KeyCode.S))
+//        {
+//            discreteActionsOut[0] = 2;
+//        }
+//    }
 
-    void OnCollisionEnter(Collision col)
-    {
-        // Reached target
-        if (col.transform.CompareTag("Wall") || col.transform.CompareTag("Runner") || col.transform.CompareTag("NPC"))
-        {
-            AddReward(-0.1f);
-        }
-    }
+//    void OnCollisionEnter(Collision col)
+//    {
+//        // Reached target
+//        if (col.transform.CompareTag("Wall") || col.transform.CompareTag("Runner") || col.transform.CompareTag("NPC"))
+//        {
+//            AddReward(-0.1f);
+//        }
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("WLTARGET"))
-        {
-            AddReward(1.0f);
-            resetTargetPos = true;
-            strike++;
-            EndEpisode();
-        }
-    }
-
-
-    [SerializeField] private LayerMask layerMask = 0;
-    [SerializeField] private float wanderRange = 30.0f;
-    private Vector3 RandomRayCast()
-    {
-        RaycastHit hit;
-        Vector3 origin = transform.position + Random.insideUnitSphere * wanderRange;
-
-        origin.y = 100.0f;
-        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.down), out hit, 200.0f, layerMask))
-        {
-            Debug.DrawRay(origin, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-        }
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("WLTARGET"))
+//        {
+//            AddReward(1.0f);
+//            resetTargetPos = true;
+//            strike++;
+//            EndEpisode();
+//        }
+//    }
 
 
-        if (hit.point == new Vector3(0.0f, 0.0f, 0.0f))
-        {
-            return RandomRayCast();
-        }
-        else return (hit.point);
-    }
-}
+//    [SerializeField] private LayerMask layerMask = 0;
+//    [SerializeField] private float wanderRange = 30.0f;
+//    private Vector3 RandomRayCast()
+//    {
+//        RaycastHit hit;
+//        Vector3 origin = transform.position + Random.insideUnitSphere * wanderRange;
+
+//        origin.y = 100.0f;
+//        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.down), out hit, 200.0f, layerMask))
+//        {
+//            Debug.DrawRay(origin, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+//        }
+
+
+//        if (hit.point == new Vector3(0.0f, 0.0f, 0.0f))
+//        {
+//            return RandomRayCast();
+//        }
+//        else return (hit.point);
+//    }
+//}
--- Assets/Scripts/MachineLearning/MachineLearning.cs
+++ Assets/Scripts/MachineLearning/MachineLearning.cs
@@ -1,119 +1,119 @@
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.MLAgents;
-using Unity.MLAgents.Sensors;
-using Unity.MLAgents.Actuators;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.MLAgents;
+//using Unity.MLAgents.Sensors;
+//using Unity.MLAgents.Actuators;
 
-public class MachineLearning : Agent
-{
-    Rigidbody rBody;
+//public class MachineLearning : Agent
+//{
+//    Rigidbody rBody;
 
-    void Start()
-    {
-        rBody = GetComponent<Rigidbody>();
-    }
+//    void Start()
+//    {
+//        rBody = GetComponent<Rigidbody>();
+//    }
 
-    public Transform Target;
+//    public Transform Target;
 
-    private bool resetTargetPos = false;
+//    private bool resetTargetPos = false;
 
-    public override void OnEpisodeBegin()
-    {
-        // If the Agent fell, zero its momentum
-        this.rBody.angularVelocity = Vector3.zero;
-        this.rBody.velocity = Vector3.zero;
-        this.transform.localPosition = new Vector3(-7, 1.5f, 0);
+//    public override void OnEpisodeBegin()
+//    {
+//        // If the Agent fell, zero its momentum
+//        this.rBody.angularVelocity = Vector3.zero;
+//        this.rBody.velocity = Vector3.zero;
+//        this.transform.localPosition = new Vector3(-7, 1.5f, 0);
 
-        //target new pos
-        //Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
-        if(resetTargetPos)
-        {
-            Target.localPosition = RandomRayCast();
-            resetTargetPos = false;
-        }
+//        //target new pos
+//        //Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+//        if(resetTargetPos)
+//        {
+//            Target.localPosition = RandomRayCast();
+//            resetTargetPos = false;
+//        }
         
-    }
+//    }
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        // Target and Agent positions
-        sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
+//    public override void CollectObservations(VectorSensor sensor)
+//    {
+//        // Target and Agent positions
+//        sensor.AddObservation(Target.localPosition);
+//        sensor.AddObservation(this.transform.localPosition);
 
-        // Agent velocity
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
-    }
+//        // Agent velocity
+//        sensor.AddObservation(rBody.velocity.x);
+//        sensor.AddObservation(rBody.velocity.z);
+//    }
 
-    public float forceMultiplier = 10;
-    public override void OnActionReceived(ActionBuffers actionBuffers)
-    {
-        // Actions, size = 2
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * forceMultiplier);
+//    public float forceMultiplier = 10;
+//    public override void OnActionReceived(ActionBuffers actionBuffers)
+//    {
+//        // Actions, size = 2
+//        Vector3 controlSignal = Vector3.zero;
+//        controlSignal.x = actionBuffers.ContinuousActions[0];
+//        controlSignal.z = actionBuffers.ContinuousActions[1];
+//        rBody.AddForce(controlSignal * forceMultiplier);
 
-        // Rewards
-        //float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+//        // Rewards
+//        //float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
 
-        // Reached target
-        //if (distanceToTarget < 1.42f)
-        //{
-        //    SetReward(1.0f);
-        //    EndEpisode();
-        //}
-
-
-    }
-
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        // Reached target
-        if (col.transform.CompareTag("Wall"))
-        {
-            //SetReward(-1.0f);
-            EndEpisode();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == Target.gameObject)
-        {
-
-            SetReward(1.0f);
-            resetTargetPos = true;
-            EndEpisode();
-        }
-    }
+//        // Reached target
+//        //if (distanceToTarget < 1.42f)
+//        //{
+//        //    SetReward(1.0f);
+//        //    EndEpisode();
+//        //}
 
 
-    [SerializeField] private LayerMask layerMask = 0;
-    [SerializeField] private float wanderRange = 30.0f;
-    private Vector3 RandomRayCast()
-    {
-        RaycastHit hit;
-        Vector3 origin = transform.position + Random.insideUnitSphere * wanderRange;
+//    }
 
-        origin.y = 100.0f;
-        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.down), out hit, 200.0f, layerMask))
-        {
-            Debug.DrawRay(origin, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-        }
+//    public override void Heuristic(in ActionBuffers actionsOut)
+//    {
+//        var continuousActionsOut = actionsOut.ContinuousActions;
+//        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+//        continuousActionsOut[1] = Input.GetAxis("Vertical");
+//    }
+
+//    void OnCollisionEnter(Collision col)
+//    {
+//        // Reached target
+//        if (col.transform.CompareTag("Wall"))
+//        {
+//            //SetReward(-1.0f);
+//            EndEpisode();
+//        }
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if(other.gameObject == Target.gameObject)
+//        {
+
+//            SetReward(1.0f);
+//            resetTargetPos = true;
+//            EndEpisode();
+//        }
+//    }
 
 
-        if (hit.point == new Vector3(0.0f, 0.0f, 0.0f))
-        {
-            return RandomRayCast();
-        }
-        else return (hit.point);
-    }
-}
+//    [SerializeField] private LayerMask layerMask = 0;
+//    [SerializeField] private float wanderRange = 30.0f;
+//    private Vector3 RandomRayCast()
+//    {
+//        RaycastHit hit;
+//        Vector3 origin = transform.position + Random.insideUnitSphere * wanderRange;
+
+//        origin.y = 100.0f;
+//        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.down), out hit, 200.0f, layerMask))
+//        {
+//            Debug.DrawRay(origin, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+//        }
+
+
+//        if (hit.point == new Vector3(0.0f, 0.0f, 0.0f))
+//        {
+//            return RandomRayCast();
+//        }
+//        else return (hit.point);
+//    }
+//}
